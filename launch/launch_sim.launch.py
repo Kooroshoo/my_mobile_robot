0f4da0159a8dadf2d9d19bd81d1b410a1886873b
kooroshoo <kooroshoo@yahoo.com>
2026-03-16 16:16:03 +0100

    add depth camera

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -71,7 +71,7 @@ def generate_launch_description():
     ros_gz_image_bridge = Node(
         package="ros_gz_image",
         executable="image_bridge",
-        arguments=["/camera/image_raw"]
+        arguments=["/camera/image_raw", "/camera/depth_image"]
     )
 
 
